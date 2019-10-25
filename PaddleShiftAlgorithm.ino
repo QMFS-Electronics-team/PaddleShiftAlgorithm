9934fce9269ea869e9e5db17cb58229d49a81132
Bhargav <bhargav22478@gmail.com>
2019-10-25 02:42:08 +0100

    Added Code for Shiftinge - Bhargav

--- PaddleShiftAlgorithm.ino
+++ PaddleShiftAlgorithm.ino
@@ -1,16 +1,29 @@
 #include <Servo.h> // Servo Motor Library
 
 Servo servo; // Declare the servo
-int analogPin = A0; // The pin to use for reading the potentiometer
+int analogPinE = A0; // The pin to use for reading the potentiometer
+int analogPinD = A1;
+int solenoidPin = A2; // The pin to write to the solenoid
+int interruptPinR = A3; // The pin which will provide the interrupt
+int interruptPinL = A4;
+
+int paddleFlag;
+int biasE;
+int biasD;
 
 
 void setup() {
-  // put your setup code here, to run once:
-
-  // Setup the analogue pin and servo  
-  pinMode(analogPin,INPUT); // Config analogue pin for input
+  // Setup the analogue pins and servo  
+  pinMode(analogPinE,INPUT);
+  pinMode(analogPinD,INPUT);
   servo.attach(9); // Servo is attached to pin 9
 
+  // Setup the solenoid pin 
+  pinMode(solenoidPin, OUTPUT);
+
+  // Setup the interrupt
+  attachInterrupt(digitalPinToInterrupt(interruptPinR),paddleUp_ISR, CHANGE); // UpShift
+  attachInterrupt(digitalPinToInterrupt(interruptPinL),paddleUp_ISR, CHANGE); // DownShift
   
   Serial.begin(9600); // this will be used to print out debug information 
   // initialize digital pin LED_BUILTIN as an output.
@@ -23,18 +36,81 @@ void loop() {
   digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
   delay(1000);                       // wait for a second
   digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       
-  servoControl();
+  delay(1000); 
+  // Interrupt signal from paddel
+  // Get both biases
+  shiftControl(paddleFlag, biasE, biasD); // Control Gear
+                        
 
   //mughees to include the code below this comment
-  
+}
+
+void paddleUp_ISR() // Need to see if this can be implemented
+{
+  paddleFlag = 1;
+}
+ 
+void paddleDown_ISR()
+{
+  paddleFlag = 0;
+}
+
+void shiftControl(int shift, int biasE, int biasD)
+{
+  clutchControl(1, biasE, biasD);
+  solenoidControl(1);
+  delay(1000); // Need to add verification of gear selection 
+  solenoidControl(0);
+  clutchControl(0, biasE, biasD);
+}
+
+void clutchControl(int engage, int biasE, int biasD)
+{
+  if(engage == 1)
+  {
+    servoControl(biasE,engage);
+  }
+  else if(engage == 0); 
+  {
+    servoControl(biasD,engage);
+  }
   
 }
 
-void servoControl(){
-  int poten = analogRead(A0); 
-  Serial.print("Potentiometer Value:");
-  Serial.println(poten); 
-  servo.write(map(poten,0,1023,0,2018)); // Map 0-1023 with 0-180 (rotation)
-    
-} 
+void servoControl(int bias, int rotationDirection)
+{
+  Serial.print("Bias Value:");
+  Serial.println(bias); 
+  int angle;
+  if(rotationDirection == 1) // Engage Clutch
+  {
+    for(angle = 0; angle < 180; angle += bias)
+    {
+      servo.write(angle);
+    }
+  }
+  else if(rotationDirection == 0) // Disengage Clutch
+  {
+    for(angle = 180; angle > 0; angle -= bias)
+    {
+      servo.write(angle);
+    }
+  }
+
+}
+
+void solenoidControl(int shift)
+{
+  if(shift == 0) //Netural
+  {
+    digitalWrite(solenoidPin, LOW);
+  }
+  else if(shift == 1) // Push
+  {
+    digitalWrite(solenoidPin, HIGH);
+  }
+  else if(shift == 2) // Pull
+  {
+    // Need to figure out how the solenoid will pull
+  }
+}
